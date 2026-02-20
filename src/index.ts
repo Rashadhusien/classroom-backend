aaf8cffc67d27f3c9bf0ce3059b5e48570bfb29d
rashad <rashadhusein440@gmail.com>
2026-02-20 16:11:06 +0200

    Implement arcjet security

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import securityMiddleware from "./middleware/security";
 const app = express();
 const PORT = 8000;
 
+if (!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL is not defined");
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
